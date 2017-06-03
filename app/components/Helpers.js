b9fd382a20c48d340d13e20777e8d47d02601fce
Yousrat <ybanna@gmail.com>
2017-06-03 12:35:28 -0700

    fixing errors

--- app/components/Helpers.js
+++ app/components/Helpers.js
@@ -2,38 +2,26 @@
 var axios = require("axios");
 
 // Geocoder API
-var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=074c30265e97430c96c6dd9b6e7688df&q=";
+var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=b9f91d369ff59547cd47b931d8cbc56b:0:74623931&q=";
 
-// Helper functions for making API Calls
-// var Helpers = {
+//Helper functions for making API Calls
+var Helper = {
 
-//   // This function serves our purpose of running the query to geolocate.
-//   runQuery: function(searchTerm) {
+    // This function serves our purpose of running the query to geolocate.
+    runQuery: function(searchTerm) {
+        var queryURL = queryURLBase + searchTerm;
+        return axios.get(queryURL).then(function(NYTdata) {
 
-//     console.log(searchTerm);
+            var data = NYTdata.data.response;
+            var NYTresult = [];
+            for (var i = 0; i < data.docs.length; i++) {
+                NYTresult.push(data.docs[i].headline.main);
 
-//     var queryURL = queryURLBase + searchTerm;
-//     return axios.get(queryURL).then(function(response) {
-//       // If get get a result, return that result's formatted address property
-//       if(NTdata.response.docs[0].headline !== 'null'){
-
-//         return response.docs[0].headline.main;
-//       }
-//       // If we don't get any results, return an empty string
-//       return "";
-//     });
-//   }
-
-//   // This function hits our own server to retrieve the record of query results
-//   // getHistory: function() {
-//   //   return axios.get("/api");
-//   // },
-
-//   // // This function posts new searches to our database.
-//   // postHistory: function(location) {
-//   //   return axios.post("/api", { location: location });
-//   // }
-// };
+            }
+            return NYTresult;
+        });
+    }
+};
 
 // // We export the API helper
-// module.exports = Helpers;
+module.exports = Helper;
--- app/components/Results.js
+++ app/components/Results.js
@@ -2,20 +2,30 @@
 var React = require("react");
 
 var Results = React.createClass({
-  render: function() {
-    
-    return (
-    	<div className="panel panel-default" id="Results">
-      <div className="panel-heading">Results</div>
-	   		<div className="panel-body">
-	   		<h1>Articles:</h1>
-          <p>{this.props.results}</p>
-	</div>
-	  </div>  
-	 
-	  		
-		);
-  }
+    render: function() {
+    	var  articles = this.props.results.map(function(article, index){
+    		return(
+    			<li key={index}>
+    				{article} 
+    			</li>
+    			);
+    		
+    	console.log(article);
+
+
+    	}.bind(this));
+
+        return ( 
+        	<div className = "panel panel-default"
+            id = "Results">
+	            <div className = "panel-heading" >Results </div> 
+	            <div className = "panel-body" >
+	            	<h1> Articles: </h1> 
+	            	<ul>{articles} </ul> 
+	            </div> 
+            </div>  
+        );
+    }
 });
 
 module.exports = Results;
