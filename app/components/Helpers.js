13d80ed901352a041b3d9eb39c6d973b7e02b432
Yousrat <ybanna@gmail.com>
2017-06-03 09:33:47 -0700

    adding logic

--- app/components/Helpers.js
+++ app/components/Helpers.js
@@ -5,36 +5,35 @@ var axios = require("axios");
 var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=074c30265e97430c96c6dd9b6e7688df&q=";
 
 // Helper functions for making API Calls
-var helper = {
+// var Helpers = {
 
-  // This function serves our purpose of running the query to geolocate.
-  runQuery: function(searchTerm) {
+//   // This function serves our purpose of running the query to geolocate.
+//   runQuery: function(searchTerm) {
 
-    console.log(searchTerm);
+//     console.log(searchTerm);
 
-    // Figure out the geolocation
-    var queryURL = queryURLBase + searchTerm;
-    return axios.get(queryURL).then(function(response) {
-      // If get get a result, return that result's formatted address property
-      if(NTdata.response.docs[0].headline !== 'null'){
+//     var queryURL = queryURLBase + searchTerm;
+//     return axios.get(queryURL).then(function(response) {
+//       // If get get a result, return that result's formatted address property
+//       if(NTdata.response.docs[0].headline !== 'null'){
 
-        return response.docs[0].headline.main;
-      }
-      // If we don't get any results, return an empty string
-      return "";
-    });
-  },
-
-  // This function hits our own server to retrieve the record of query results
-//   getHistory: function() {
-//     return axios.get("/api");
-//   },
-
-//   // This function posts new searches to our database.
-//   postHistory: function(location) {
-//     return axios.post("/api", { location: location });
+//         return response.docs[0].headline.main;
+//       }
+//       // If we don't get any results, return an empty string
+//       return "";
+//     });
 //   }
+
+//   // This function hits our own server to retrieve the record of query results
+//   // getHistory: function() {
+//   //   return axios.get("/api");
+//   // },
+
+//   // // This function posts new searches to our database.
+//   // postHistory: function(location) {
+//   //   return axios.post("/api", { location: location });
+//   // }
 // };
 
-// We export the API helper
-module.exports = helper;
+// // We export the API helper
+// module.exports = Helpers;
--- app/components/Main.js
+++ app/components/Main.js
@@ -4,7 +4,7 @@ var Header = require("./Header");
 var Search = require("./Search");
 var Results = require("./Results");
 var Saved = require("./Saved");
-var Helpers = require("./Helpers");
+//var Helpers = require("./Helpers");
 
 var Main = React.createClass({
   
@@ -12,31 +12,31 @@ getInitialState: function() {
     return { searchTerm: "", results: "", saved: "" };
   },
 
-componentDidUpdate: function() {
+// componentDidUpdate: function() {
 
-    // Run the query for the address
-    Helpers.runQuery(this.state.searchTerm).then(function(data) {
-      if (data !== this.state.results) {
-        console.log("article header:", data);
-        this.setState({ results: data });
+//     // Run the query for the address
+//     Helpers.runQuery(this.state.searchTerm).then(function(data) {
+//       if (data !== this.state.results) {
+//         console.log("article header:", data);
+//         this.setState({ results: data });
 
-        // After we've received the result... then post the search term to our history.
-        // Helpers.postHistory(this.state.searchTerm).then(function() {
-        //   console.log("Updated!");
+//         // After we've received the result... then post the search term to our history.
+//         // Helpers.postHistory(this.state.searchTerm).then(function() {
+//         //   console.log("Updated!");
 
-        //   // After we've done the post... then get the updated history
-        //   helpers.getHistory().then(function(response) {
-        //     console.log("Current History", response.data);
+//         //   // After we've done the post... then get the updated history
+//         //   helpers.getHistory().then(function(response) {
+//         //     console.log("Current History", response.data);
 
-        //     console.log("History", response.data);
+//         //     console.log("History", response.data);
 
-        //     this.setState({ history: response.data });
+//         //     this.setState({ history: response.data });
 
-         // }.bind(this));
-        //}.bind(this));
-      }
-    }.bind(this));
-  },
+//          // }.bind(this));
+//         //}.bind(this));
+//       }
+//     }.bind(this));
+//   },
 
 
 setTerm: function(term) {
